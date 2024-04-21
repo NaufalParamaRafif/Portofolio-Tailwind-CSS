2e94cfb838bea6d2f2f80b57af0bc646a668a6de
NaufalParamaRafif <profesoraiueo@gmail.com>
2024-04-21 20:06:00 +0700

    Finished About Section

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,11 +9,12 @@ module.exports = {
     extend: {
       colors: {
         primary: '#14b8a6',
+        secondary: '#64748b',
         dark: '#0f172a',
       },
       screens: {
         '2xl' : '1320px',
-        
+
       }
     },
   },
